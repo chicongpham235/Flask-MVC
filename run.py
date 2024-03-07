399935748e3859315f63e67486777a3d58cee52f
Chí Công <cong.pc2305@gmail.com>
2024-03-07 12:40:39 +0700

    update

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     parser=argparse.ArgumentParser()
     parser.add_argument("-P", "--port", help="Port of backend Server", default=8000)
     args = parser.parse_args()
-    port = args.port or 8000
+    port = args.port
     app.run(host="localhost", port=port, debug=True)
