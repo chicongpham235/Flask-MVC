ee055ca504494f7e9b84a6c9dd7bd473517321ea
Chí Công <cong.pc2305@gmail.com>
2024-03-07 12:34:00 +0700

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+# README
+
+## Install packages
+
+```shell
+pip install -r ./requirements.txt
+```
+
+## Run localhost
+
+```
+python run.py --port=<PORT_NUMBER>
+```
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from __init__ import app
+import argparse
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True)
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-P", "--port", help="Port of backend Server", default=8000)
+    args = parser.parse_args()
+    port = args.port or 8000
+    app.run(host="localhost", port=port, debug=True)
