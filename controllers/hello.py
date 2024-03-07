09b89684788998c767bddc537a9173951a0b17f8
Chí Công <cong.pc2305@gmail.com>
2024-03-07 12:09:35 +0700

    update

---
+++ controllers/hello.py
@@ -0,0 +1,18 @@
+"""
+    Example Controllers
+"""
+
+from __init__ import app
+from flask import render_template, redirect, url_for
+"""
+    Import Models
+from models.Hello import Hello
+"""
+#route index
+@app.route('/', methods = ['GET'])
+def index():
+    data = {
+        "title": "Hello World",
+        "body": "Flask simple MVC"
+    }
+    return render_template('index.html.j2', data = data)
---
+++ models/Hello.py
@@ -0,0 +1,15 @@
+import mysql.connector, json
+"""
+    Import local package
+
+from config.Database import connection as con, cursor as cur
+from config.DatetimeEncoder import DatetimeEncoder
+from config.Hash import Hash
+"""
+"""
+    Your Code
+"""
+class Hello:
+
+    def __init__(self):
+        pass
